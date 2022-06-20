c9ebc4709c7a507a3cba2809c8049973bc909ac1
Alex Grey <Alex.Grey@softwire.com>
2022-06-20 17:26:36 +0100

    fizzbyzzv2

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,21 @@
-for (let i = 0; i <= 100; i++) {
-    if (i%15==0) {
-        console.log("FizzBuzz")
-    } else if (i%3==0) {
-        console.log("Fizz")
-    } else if (i%5==0) {
-              console.log("Buzz")
-    } else  {
-            console.log(i)
-      }
-   }
+for (let number = 250; number <= 255; number++) {
+    let buzzy = ""
+    let i = 0
+    let divisibility = [number%3+(1-Math.min(1,number%11)),number%13,number%5+(1-Math.min(1,number%11)),number%7+(1-Math.min(1,number%11)),number%11,Math.min(number%17,1)] // Fizz,Fezz,Buzz,Bang,Bong,Reverse
+    if (divisibility[5] === 0) {
+        divisibility[5] = 4
+        i = 4
+    }
+    let fizz = ["Fizz","Fezz","Buzz","Bang","Bong"]
+    for (let j = 0; j <= 4; j++) {
+        if (divisibility[i]==0) {
+            buzzy += fizz[i]
+        }
+        i = (i+divisibility[5])%5
+    }
+    if (buzzy === "") {
+        buzzy += number
+    }
+    console.log(buzzy)
+}
+
